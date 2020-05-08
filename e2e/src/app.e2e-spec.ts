7445e5e06079d34ede50614404ffedfe2a5eaa6b
MiguelSilvaP22 <miguel.silva22@inacapmail.cl>
2020-05-08 19:08:38 -0400

    primer commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('proyect-multiplica app is running!');
+    expect(page.getTitleText()).toEqual('proyectMultiplica app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proyect-multiplica'`, () => {
+  it(`should have as title 'proyectMultiplica'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyect-multiplica');
+    expect(app.title).toEqual('proyectMultiplica');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('proyect-multiplica app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('proyectMultiplica app is running!');
   });
 });
